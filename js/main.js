bfc392c557028ec3c505d1ef124eddb306aff515
powerpaul17 <paultirk@paultirk.com>
2016-12-06 21:36:52 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * ownCloud - paperwork
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Paul Tirk <paultirk@paultirk.com>
+ * @copyright Paul Tirk 2016
+ */
+
+angular.module('paperworkApp', ['uuid4', 'angular-cache', 'ngRoute', 'ui.bootstrap', 'ui.select', 'ngSanitize', 'ngclipboard'])
+.config(function($routeProvider) {
+
+	$routeProvider.when('/:gid', {
+		template: '<contactdetails></contactdetails>'
+	});
+
+	$routeProvider.when('/:gid/:uid', {
+		template: '<contactdetails></contactdetails>'
+	});
+
+	$routeProvider.otherwise('/' + t('paperwork', 'All papers'));
+
+});
